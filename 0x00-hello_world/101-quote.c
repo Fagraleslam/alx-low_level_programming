eb30a2e1893d01c9f446aa221652fb3f8f422c8a
Fagraleslam <fagraleslam.hussein@gmail.com>
2023-09-07 13:43:39 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
  * Description: A rogram that prints line in error
  * 
- * Return: 1 (Success)
+ * Return: 1 (not Success)
  *
  */
 int main(void)
 {
-	fputs(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (10);
+	char quo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1,quo,59);
+	return (1);
 }
