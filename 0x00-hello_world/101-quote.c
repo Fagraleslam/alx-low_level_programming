bd2610fe5d8765dc3869329515bd9d597a88f509
Fagraleslam <fagraleslam.hussein@gmail.com>
2023-09-07 13:32:50 -0700

    h

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: A rogram that prints line in error
+ * 
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	fput(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (10);
+}
