2b828b1489aa3135c62fc423394f6c55f3448579
Fagraleslam <fagraleslam.hussein@gmail.com>
2023-09-11 11:30:19 -0700

    l

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main: entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * EReturn: 0 (success)
+*/
+
+int main(void)
+{
+	int a = 1, b;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a + 48);
+				putchar(b + 48);
+
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
