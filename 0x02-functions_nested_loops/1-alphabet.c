88605fd1d0581882bd92b04003606093b6ebcc34
Fagraleslam <fagraleslam.hussein@gmail.com>
2023-09-12 13:45:00 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  * Return: 0 (success)
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch < 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
 
